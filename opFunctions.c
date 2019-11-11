89d68cb3889e3c41e5da14aba32542745d5ff12a
volpestyle <volpestyle@gmail.com>
2019-11-10 18:56:16 -0600

    Fix

--- opFunctions.c
+++ opFunctions.c
@@ -83,35 +83,35 @@ void dump(u_int64_t *regArr, u_int64_t *memory, u_int64_t *stack, FILE *f)
 
         if (i == 16)
         {
-            printf("(IPO)  X%ld: %d\n", i, regArr[i]);
+            printf("%" PRIu64 "\n", i, regArr[i]);
         }
         else if (i == 17)
         {
-            printf("(IP1)  X%ld: %d\n", i, regArr[i]);
+            printf("(IP1)  X%d: %d\n", i, regArr[i]);
         }
         else if (i == 28)
         {
-            printf(" (SP)  X%ld: %d\n", i, regArr[i]);
+            printf(" (SP)  X%d: %d\n", i, regArr[i]);
         }
         else if (i == 29)
         {
-            printf(" (FP)  X%ld: %d\n", i, regArr[i]);
+            printf(" (FP)  X%d: %d\n", i, regArr[i]);
         }
         else if (i == 30)
         {
-            printf(" (LR)  X%ld: %d\n", i, regArr[i]);
+            printf(" (LR)  X%d: %d\n", i, regArr[i]);
         }
         else if (i == 31)
         {
-            printf("(XZR)  X%ld: %d\n", i, regArr[i]);
+            printf("(XZR)  X%d: %d\n", i, regArr[i]);
         }
         else if (i < 10)
         {
-            printf("       X%ld:  %d\n", i, regArr[i]);
+            printf("       X%d:  %d\n", i, regArr[i]);
         }
         else
         {
-            printf("       X%ld: %d\n", i, regArr[i]);
+            printf("       X%d: %d\n", i, regArr[i]);
         }
     }
     prnl();
