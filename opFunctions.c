bb567fd23212aa979a221f71fa53eee3da2cd2fb
volpestyle <volpestyle@gmail.com>
2019-11-10 18:58:21 -0600

    Fix

--- opFunctions.c
+++ opFunctions.c
@@ -83,7 +83,7 @@ void dump(u_int64_t *regArr, u_int64_t *memory, u_int64_t *stack, FILE *f)
 
         if (i == 16)
         {
-            printf("%" PRIu64 "\n", i, regArr[i]);
+            printf("(IPO)  X%h: %h\n", i, (int)regArr[i]);
         }
         else if (i == 17)
         {
